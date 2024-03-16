b79d16fef08f76883298be1adc98a2d8d11dc9c2
Gabriel <117124925+Gabriel-Cabero-Torrico@users.noreply.github.com>
2024-03-16 19:57:54 -0400

    Estratura para la navegacion de la paginas

---
+++ frontend/src/Ambientes/homeUno.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const homeUno = () => {
+  return (
+    <div>homeUno</div>
+  )
+}
---
+++ frontend/src/nombre_docente/Formulario_Registro.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const formulario_Registro = () => {
+  return (
+    <div>formulario_Registro</div>
+  )
+}
