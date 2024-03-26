d9aeba8aecf98258dcb4bd2b4bc7f6870f90e299
Cleysi <cleysi.chamaca@gmail.com>
2024-03-26 11:02:52 -0400

    Creacion de tablas

--- backend/app/Models/Administrador.php
+++ backend/app/Models/Administrador.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Administrador extends Model
 {
     use HasFactory;
+    protected $table = 'administrador';
+    protected $fillable = [
+        "id_usuario",
+        "codigo_administrador",
+        "estado_administrador"
+    ];
 }
