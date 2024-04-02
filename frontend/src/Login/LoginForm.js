d09849604f3a645755c89ea1a362eabd50aea23c
Cleysi <cleysi.chamaca@gmail.com>
2024-04-02 13:05:57 -0400

    Landing-Longin Responsivo

--- frontend/src/Login/LoginForm.js
+++ frontend/src/Login/LoginForm.js
@@ -1,11 +1,14 @@
-import { useCallback } from "react";
-import "./LoginForm.css";
-import { Link } from 'react-router-dom';
-import { FaAt, FaLock} from 'react-icons/fa';
+import { useCallback, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars,FaAt, FaLock } from "react-icons/fa"; 
+import { Collapse, Row,Col,Container } from "react-bootstrap";
 import logo from "../assets/logoSIS.png";
-import logop from "../assets/logosol-1@2x.png"
+import logop from "../assets/logosol-1@2x.png";
+import "./LoginForm.css";
 
 const LoginForm = () => {
+  const [menuClicked, setMenuClicked] = useState(false);
+
   const onPricingClick = useCallback(() => {
     window.location.href = "https://www.umss.edu.bo/";
   }, []);
@@ -13,87 +16,103 @@ const LoginForm = () => {
   const onSupportClick = useCallback(() => {
     window.location.href = "http://www.fcyt.umss.edu.bo/";
   }, []);
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
   return (
-    <div className="hero-06">
-      <div className="nav-bare">
-        <button className="logon">
-          <img className="image-1-icon" alt="logo" src={logo}/>
-          <div className="ovonrueden">SIRA</div>
-        </button>
-        <div className="menus">
-          <button className="about">Acerca de</button>
-          <button className="pricing" onClick={onPricingClick}>UMSS</button>
-          <button className="pricing" onClick={onSupportClick}>FCYT</button>
-        </div>
-      </div>
-      <div className="content">
-            <div>
-                <Link to="/" className="logo"> 
-                    <img src={logo} alt="" /> 
+    <div className="hero-6">
+        <div>
+            <nav className="navbar navbar-expand-lg nav-bare">
+                <Link to="/" className="navbar-brand logon">
+                    <img className="image-1-icon" alt="logo" src={logo} />
+                    <span className="ovonrueden">SIRA</span>
                 </Link>
-                <div style={{ height: '10vh' }}></div>
-                <div className="section">
-                    <div className="container-fluid d-flex justify-content-center align-items-center full-height">
-                        <div className="row full-height justify-content-center">
-                            <div className="col-12 text-center align-self-center py-5">
-                                <div className="section pb-5 pt-5 pt-sm-2 text-center">
-                                    <h6 className="mb-0 pb-3" style={{ paddingLeft: '85px' }}>
-                                        <span className="option">Inicia sesión</span>
-                                        <span> </span>
-                                        <span className="option">Recuperar Contraseña</span>
-                                    </h6>
-                                    <input className="checkbox" type="checkbox" id="reg-log" name="reg-log" />
-                                    <label htmlFor="reg-log"></label>
-                                    <div className="card-3d-wrap mx-auto">
-                                        <div className="card-3d-wrapper">
-                                            <div className="card-front">
-                                                <div className="center-wrap">
-                                                    <div className="section text-center">
-                                                        <h4 className="mb-4 pb-3">Inicio sesión</h4>
-                                                        <div className="form-group">
-                                                            <input type="email" name="logemail" className="form-style" placeholder="Tu Correo" id="logemail" autoComplete="off" />
-                                                            <FaAt className="input-icon" />
-                                                        </div>
-                                                        <div className="form-group mt-2">
-                                                            <input type="password" name="logpass" className="form-style" placeholder="Tu Contraseña" id="logpass" autoComplete="off" />
-                                                            <FaLock className="input-icon" />
-                                                        </div>
-                                                        <Link to="../inicio/HomeUno">
-                                                            <div className="botones mt-4" >Ingresar</div>
-                                                        </Link>
-                                                        <p className="mb-0 mt-4 text-center"><a href="#0" className="link">¿Olvidaste tu Contraseña?</a></p>
-                                                    </div>
-                                                </div>
+                <button className={`navbar-toggler ${menuClicked ? "active" : ""}`}
+                    type="button"
+                    onClick={() => {
+                    setMenuClicked(!menuClicked);
+                    setMenuOpen(!menuOpen);
+                    }}
+                    aria-label="Toggle navigation"
+                >
+                <span className="navbar-toggler-icon menu-icono">
+                <FaBars />
+                 </span>
+                </button>
+                    <Collapse in={menuOpen}>
+                        <div className="navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav ml-auto menus">
+                                <li className="nav-item">
+                                    <button className="nav-link about">Acerca de</button>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="nav-link pricing" onClick={onPricingClick}>UMSS</button>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="nav-link pricing" onClick={onSupportClick}>FCYT</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </Collapse>
+            </nav>
+        </div>
+        <div className="content">
+            <Row className="justify-content-center">
+                <Col md={6} > 
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col xs={12} sm={10} md={8} lg={6}> {/* Define el ancho del formulario en diferentes tamaños de pantalla */}
+                                <div className="login-box">
+                                    <div className="section">
+                                        <h6 className="mb-0 pb-3 text-center">Inicia sesión</h6>
+                                        <form onSubmit={handleSubmit}>
+                                            <div className="form-group">
+                                                <input
+                                                    type="email"
+                                                    name="logemail"
+                                                    className="form-control"
+                                                    placeholder="Tu Correo"
+                                                    value={email}
+                                                    onChange={(e) => setEmail(e.target.value)}
+                                                />
+                                                <FaAt className="input-icon" />
                                             </div>
-                                            <div className="card-back">
-                                                <div className="center-wrap">
-                                                    <div className="section text-center">
-                                                        <h4 className="mb-4 pb-3">Recuperar Contraseña</h4>
-                                                        <div className="form-group">
-                                                            <input type="email" name="logemail" className="form-style" placeholder="Correo Institucional" id="logemail1" autoComplete="off" />
-                                                            <FaAt className="input-icon" />
-                                                        </div>
-                                                        <button className="botones mt-4" >Enviar Codigo</button>
-                                                    </div>
-                                                </div>
+                                            <div className="form-group">
+                                                <input
+                                                    type="password"
+                                                    name="logpass"
+                                                    className="form-control"
+                                                    placeholder="Tu Contraseña"
+                                                    value={password}
+                                                    onChange={(e) => setPassword(e.target.value)}
+                                                />
+                                                <FaLock className="input-icon" />
+                                               
+                                                
                                             </div>
-                                        </div>
+                                            <button className="btn btn-primary btn-block">Ingresar</button>
+                                            <p className="mt-3 text-center">
+                                                    <Link to="/forgot-password" className="link">¿Olvidaste tu Contraseña?</Link>
+                                                </p>
+                                           
+                                        </form>
+                                        
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <div className="design-parent">
-          <div className="design">
-            <img className="vector-icon" alt="" src="/vector.svg" />
-          </div>
-          <img className="logo-sol-1-icon" alt="" src={logop} />
+                            </Col>
+                        </Row>
+                    </Container>
+                </Col>
+                <Col md={6}> {/* La segunda columna ocupará la mitad del ancho en dispositivos medianos y superiores */}
+                    <div>Contenido de la segunda columna</div>
+                </Col>
+            </Row>
         </div>
-      </div>
-      <footer className="image" />
     </div>
   );
 };
