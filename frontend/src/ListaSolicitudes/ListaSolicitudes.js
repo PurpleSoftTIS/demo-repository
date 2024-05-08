725e6a6022dc15bb1abcdb2ad04d74e09a6382e9
joaquinix29 <129318069+joaquinix29@users.noreply.github.com>
2024-05-07 15:12:18 -1200

    listas

--- frontend/src/ListaSolicitudes/ListaSolicitudes.js
+++ frontend/src/ListaSolicitudes/ListaSolicitudes.js
@@ -15,94 +15,103 @@ const ListaSolicitudes = () => {
     Motivo: "",
     Tipo_de_solicitud: ""
   });
- 
+  const [busqueda, setBusqueda] = useState('');
+
   const aceptarsolicitud = (id_solicitud) => {
-      fetch(`http://127.0.0.1:8000/api/aceptarsolicitud/${id_solicitud}`, {
-          method: 'PUT',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-      })
-          .then((response) => {
-              if (!response.ok) {
-                  throw new Error('Error al cambiar el estado de la solicitud');
-              }
-              console.log('Solicitud actualizada:', response);
-              return response.json();
-          })
-          .then((data) => {
-              console.log('Datos de la solicitud actualizada:', data);
-          })
-          .catch((error) => {
-              console.error(error);
-          });
-  };
-
-  const rechazarsolicitud= (id_solicitud) => {
-    fetch(`http://127.0.0.1:8000/api/rechazarsolicitud/${id_solicitud}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
+    fetch(`http://127.0.0.1:8000/api/aceptarsolicitud/${id_solicitud}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Error al cambiar el estado de la solicitud');
-            }
-            console.log('Solicitud actualizada:', response);
-            return response.json();
-        })
-        .then((data) => {
-            console.log('Datos de la solicitud actualizada:', data);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error al cambiar el estado de la solicitud');
+        }
+        console.log('Solicitud actualizada:', response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Datos de la solicitud actualizada:', data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
+  const rechazarsolicitud = (id_solicitud) => {
+    fetch(`http://127.0.0.1:8000/api/rechazarsolicitud/${id_solicitud}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error al cambiar el estado de la solicitud');
+        }
+        console.log('Solicitud actualizada:', response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Datos de la solicitud actualizada:', data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   useEffect(() => {
-      fetch('http://127.0.0.1:8000/api/obtenerSol', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
+    fetch('http://127.0.0.1:8000/api/obtenerSol', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        setSolicitudes(data);
       })
-        .then(response => response.json())
-        .then(data => {
-          setSolicitudes(data);
-        })
-        .catch(error => console.error('Error al obtener los datos:', error));
+      .catch(error => console.error('Error al obtener los datos:', error));
   }, []);
 
   const mostrarFormularioParaSolicitud = (solicitud) => {
-      setFormData({
-        docente: solicitud.nombre,
-        materia: solicitud.nombre_materia,
-        grupo: solicitud.grupo,
-        aula: solicitud.nombre_ambiente,
-        capacidad: solicitud.numero_estudiantes,
-        tipo_de_solicitud: solicitud.tipo_solicitud,
-        fecha: solicitud.fecha_solicitud,
-        hora: solicitud.hora_inicio,
-        motivo: solicitud.motivo
-      });
-      setMostrarFormulario(true);
+    setFormData({
+      docente: solicitud.nombre,
+      materia: solicitud.nombre_materia,
+      grupo: solicitud.grupo,
+      aula: solicitud.nombre_ambiente,
+      capacidad: solicitud.numero_estudiantes,
+      tipo_de_solicitud: solicitud.tipo_solicitud,
+      fecha: solicitud.fecha_solicitud,
+      hora: solicitud.hora_inicio,
+      motivo: solicitud.motivo
+    });
+    setMostrarFormulario(true);
   };
 
   const cerrarFormulario = () => {
-      setMostrarFormulario(false);
+    setMostrarFormulario(false);
   };
 
   const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData(prevState => ({
-        ...prevState,
-        [name]: value
-      }));
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
-return (
+  const filtrarSolicitudes = Array.isArray(solicitudes)
+  ? solicitudes.filter(
+      (solicitud) =>
+        solicitud.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
+        solicitud.nombre_materia.toLowerCase().includes(busqueda.toLowerCase()) ||
+        solicitud.motivo.toLowerCase().includes(busqueda.toLowerCase())
+    )
+  : [];
+
+  return (
     <div className="container" style={{ minHeight: '78.7vh' }}>
       <div style={{ height: '4vh' }}></div>
       <div
@@ -114,7 +123,12 @@ return (
       >
         <h2 style={{ margin: 0 }}>Solicitudes Todas:</h2>
         <div>
-          <input type="text" placeholder="Buscar Reserva" />
+          <input
+            type="text"
+            placeholder="Buscar Reserva"
+            value={busqueda}
+            onChange={(e) => setBusqueda(e.target.value)}
+          />
           <button className="butn butn-filtro">Filtros</button>
         </div>
       </div>
@@ -132,7 +146,7 @@ return (
           </tr>
         </thead>
         <tbody>
-          {Array.isArray(solicitudes) && solicitudes.map((solicitud, index) => (
+          {Array.isArray(filtrarSolicitudes) && filtrarSolicitudes.map((solicitud, index) => (
             <tr key={index}
               className="fila-lista"
               onClick={() => mostrarFormularioParaSolicitud(solicitud)}
@@ -142,13 +156,13 @@ return (
               <td>{solicitud.nombre_materia}</td>
               <td>{solicitud.motivo}</td>
               <td>{solicitud.fecha_solicitud}</td>
-              <td>{solicitud.hora_inicio+" "+solicitud.hora_fin}</td>
+              <td>{solicitud.hora_inicio + " " + solicitud.hora_fin}</td>
               <td>
                 <button className="btn btn-editar mr-2" onClick={() => aceptarsolicitud(solicitud.id_solicitud)}>
                   Aceptar
                 </button>
 
-                <button className="btn btn-eliminar"  onClick={() => rechazarsolicitud(solicitud.id_solicitud)} >
+                <button className="btn btn-eliminar" onClick={() => rechazarsolicitud(solicitud.id_solicitud)}>
                   Rechazar
                 </button>
               </td>
@@ -182,7 +196,7 @@ return (
         </div>
       )}
     </div>
-);
+  );
 };
 
 export default ListaSolicitudes;
