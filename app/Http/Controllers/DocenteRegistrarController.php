d582d0211f7de83b9f3b0b272d33fda5f6bb935d
Cristian <abcristianfloreslu@gmail.com>
2024-06-24 08:01:31 -0400

    Parche Final URW

--- app/Http/Controllers/DocenteRegistrarController.php
+++ app/Http/Controllers/DocenteRegistrarController.php
@@ -5,6 +5,7 @@ use App\Models\Usuario;
 use Illuminate\Http\Request;
 use App\Models\Docente;
 use Psy\Readline\Hoa\Console;
+use Illuminate\Support\Facades\Mail;
 
 class DocenteRegistrarController extends Controller
 {
@@ -32,6 +33,9 @@ class DocenteRegistrarController extends Controller
             $docente->codigo_docente = $request->input('codigoDocente');
             $docente->estado_docente = 'activo'; // Puedes definir el estado aquí
             $docente->save();
+
+            $nombreCompleto = "{$usuario->nombre} {$usuario->apellido_paterno} {$usuario->apellido_materno}";
+            $this->enviarCorreoRegistro($usuario->correo_electronico, $nombreCompleto, $contrasena);
             return response()->json(['message' => 'Docente registrado correctamente'], 201);
         } catch (\Exception $e) {
             \Log::error('Error al intentar registrar un docente: ' . $e->getMessage());
@@ -61,5 +65,30 @@ class DocenteRegistrarController extends Controller
         }
         
         return trim($morse_texto);
+    }
+    public function enviarCorreoRegistro($correoElectronico, $nombreCompleto, $contrasena)
+    {
+        // Preparar el mensaje del correo
+        $subject = 'Registro en el Sistema SIRA';
+        $message = "
+            Estimado(a) {$nombreCompleto},
+
+            El administrador de la plataforma SIRA le ha habilitado una cuenta con los siguientes datos:
+
+            Correo electrónico: {$correoElectronico}
+            Contraseña: {$contrasena}
+
+            Para iniciar sesión, ingrese al siguiente enlace:
+            https://purplesoft-production.up.railway.app/
+
+            Atentamente,
+            Administración SIRA
+        ";
+
+        // Enviar el correo
+        Mail::raw($message, function ($mail) use ($correoElectronico, $subject) {
+            $mail->to($correoElectronico)
+                ->subject($subject);
+        });
     }
 }
